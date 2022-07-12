006950d76c0d5362eb750e40db1a753ac6b41af9
Alonso del Arte <alonso.delarte@gmail.com>
2022-07-12 09:32:50 -0400

    Passing single-char tests, moving on to String tests

--- src/com/genspark/random/Pseudorandom.java
+++ src/com/genspark/random/Pseudorandom.java
@@ -8,10 +8,27 @@ import java.util.Random;
  */
 public class Pseudorandom {
 
+    private static final char PRINTABLE_ASCII_SPAN_BEGIN = ' ';
+
+    private static final char PRINTABLE_ASCII_SPAN_END = '~';
+
+    private static final int PRINTABLE_ASCII_SPAN = PRINTABLE_ASCII_SPAN_END
+            - PRINTABLE_ASCII_SPAN_BEGIN + 1;
+
     private static final Random RANDOM = new Random();
 
     public static char nextASCIIChar() {
-        return '\u4E00';
+        return (char) (' ' + RANDOM.nextInt(PRINTABLE_ASCII_SPAN));
+    }
+
+    // TODO: Write tests for this
+    public static String nextASCIICharSeq(int length) {
+        return "NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public static String nextASCIICharSeq(int minLength, int maxLength) {
+        return "NOT IMPLEMENTED YET";
     }
 
 }
--- test/com/genspark/random/PseudorandomTest.java
+++ test/com/genspark/random/PseudorandomTest.java
@@ -1,6 +1,8 @@
 package com.genspark.random;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,7 +41,28 @@ class PseudorandomTest {
     @Test
     void testNextASCIIChar() {
         System.out.println("nextASCIIChar");
-        fail("Haven't written test yet");
+        Set<Character> characters = new HashSet<>(PRINTABLE_ASCII_SPAN);
+        for (int i = 0; i < PRINTABLE_ASCII_SPAN; i++) {
+            characters.add(Pseudorandom.nextASCIIChar());
+        }
+        int expected = 55 * PRINTABLE_ASCII_SPAN / 100;
+        int actual = characters.size();
+        String msg = "Calling nextASCIIChar() " + PRINTABLE_ASCII_SPAN
+                + " times gave " + actual
+                + " distinct character(s), ought to be at least " + expected
+                + " distinct";
+        assert expected <= actual : msg;
+    }
+
+    @Test
+    void testNextASCIICharSeqOneParamGivesRightLength() {
+        for (int expected = 2; expected < 80; expected++) {
+            String s = Pseudorandom.nextASCIICharSeq(expected);
+            int actual = s.length();
+            String msg = "Pseudorandom String \"" + s
+                    + "\" should be of length " + expected;
+            assertEquals(expected, actual, msg);
+        }
     }
 
 }
